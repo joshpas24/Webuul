f85bd719a419047c59b7e011cb634f9b0413d7fe
Josh Pascual <joshapascual@gmail.com>
2023-09-14 12:13:33 -0700

    updated delete seeder file

--- app/seeds/delete.py
+++ app/seeds/delete.py
@@ -1,11 +1,22 @@
-from ..models import db, Transaction, Watchlist, Holding, WatchlistStock
+from ..models import db, Transaction, Watchlist, Holding, WatchlistStock, environment, SCHEMA
 
 
 def undo_transactions_watchlists_holdings():
     # Delete all test entries
-    Transaction.query.delete()
-    Watchlist.query.delete()
-    Holding.query.delete()
-    WatchlistStock.query.delete()
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.transactions RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.watchlists RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.holdings RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.watchlist_stocks RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM transactions")
+        db.session.execute("DELETE FROM watchlists")
+        db.session.execute("DELETE FROM holdings")
+        db.session.execute("DELETE FROM watchlist_stocks")
+
+    # Transaction.query.delete()
+    # Watchlist.query.delete()
+    # Holding.query.delete()
+    # WatchlistStock.query.delete()
 
     db.session.commit()
