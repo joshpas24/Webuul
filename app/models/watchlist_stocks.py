8a20b26058edacb1795a9a403e8bf7d3ba34cc7b
Josh Pascual <joshapascual@gmail.com>
2023-09-14 16:01:28 -0700

    added prefix for watchlist_stocks foreign keys

--- app/models/watchlist_stocks.py
+++ app/models/watchlist_stocks.py
@@ -4,8 +4,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 watchlist_stocks = db.Table(
     "watchlist_stocks",
     db.Model.metadata,
-    db.Column("watchlist_id", db.Integer, db.ForeignKey("watchlists.id"), primary_key=True),
-    db.Column("stock_id", db.Integer, db.ForeignKey("stocks.id"), primary_key=True)
+    db.Column("watchlist_id", db.Integer, db.ForeignKey(add_prefix_for_prod("watchlists.id")), primary_key=True),
+    db.Column("stock_id", db.Integer, db.ForeignKey(add_prefix_for_prod("stocks.id")), primary_key=True)
 )
 
 if environment == "production":
