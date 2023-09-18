16b232f68c661a691b9054c6871236dd62cb7bff
Josh Pascual <joshapascual@gmail.com>
2023-09-17 21:25:58 -0700

    signup page error handling fix

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -11,10 +11,13 @@ def validation_errors_to_error_messages(validation_errors):
     """
     Simple function that turns the WTForms validation errors into a simple list
     """
-    errorMessages = []
+    # errorMessages = []
+    errorMessages = {}
+    print("******validation_errors: ", validation_errors)
     for field in validation_errors:
         for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
+            # errorMessages.append(f'{field} : {error}')
+            errorMessages[field] = {error}
     return errorMessages
 
 
@@ -72,7 +75,7 @@ def sign_up():
         db.session.commit()
         login_user(user)
         return user.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    return {'errors': form.errors}, 401
 
 
 @auth_routes.route('/unauthorized')
