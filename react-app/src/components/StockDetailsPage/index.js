40caece660e51272121aeb486baea77f64b8055b
Josh Pascual <joshapascual@gmail.com>
2023-12-10 18:02:54 -0800

    details page fix 1

--- react-app/src/components/StockDetailsPage/index.js
+++ react-app/src/components/StockDetailsPage/index.js
@@ -8,6 +8,7 @@ import IndexPriceChart from "../LineChart";
 import { useNavigation } from "../../context/NavigationView";
 import "./StockDetailsPage.css"
 import LoadingComponent from "../LoadingVid";
+import background from "./adien.png"
 
 function StockDetailsPage() {
     const dispatch = useDispatch();
@@ -84,130 +85,137 @@ function StockDetailsPage() {
         <>
             {isLoaded && pricesArr ? (
                 <div className="details-page-container">
-                    <div className="details-nav">
-                        <div className="markets-back" onClick={() => history.push("/markets")}>
-                            <i class="fa-solid fa-arrow-left"> </i>
-                            <div className="markets-text">MARKETS</div>
-                        </div>
-                        <div className="details-searchbar-div">
-                            <div className="details-searchbar-container">
-                                <div className="details-searcbar-icon">
-                                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <div className="details-top">
+                        <div className="details-top-inner">
+                            <div className="details-nav">
+                                <div className="details-nav-left">
+                                    <div className="markets-back" onClick={() => history.push("/markets")}>
+                                        <div className="markets-text">MARKETS</div>
+                                    </div>
+                                    <i class="fa-solid fa-caret-right"></i>
+                                    <div>
+                                        <div className="markets-text">{symbol}</div>
+                                    </div>
                                 </div>
-                                <input
-                                    type="text"
-                                    value={searchVal}
-                                    placeholder="search by name or ticker"
-                                    onChange={(e) => setSearchVal(e.target.value)}
-                                />
+                                <div className="details-searchbar-div">
+                                    <div className="details-searchbar-container">
+                                        <div className="details-searcbar-icon">
+                                            <i class="fa-solid fa-magnifying-glass"></i>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            value={searchVal}
+                                            placeholder="search by name or ticker"
+                                            onChange={(e) => setSearchVal(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className="details-search-results">
+                                        {searchResults && searchResults.length > 0 && searchVal.length > 0 ?
+                                            searchResults.map((item) => (
+                                                <li key={item['1. symbol']} onClick={() => getStockDetails(item['1. symbol'])}>
+                                                    <div>{item['2. name']}</div>
+                                                    <div>{item['1. symbol']}</div>
+                                                </li>
+                                            )
+                                        ) : (
+                                            <li className={showSearchList ? "" : "hidden-search"}>No matches found</li>
+                                        )}
+                                    </div>
+                                </div>
                             </div>
-                            <div className="details-search-results">
-                                {searchResults && searchResults.length > 0 && searchVal.length > 0 ?
-                                    searchResults.map((item) => (
-                                        <li key={item['1. symbol']} onClick={() => getStockDetails(item['1. symbol'])}>
-                                            <div>{item['2. name']}</div>
-                                            <div>{item['1. symbol']}</div>
-                                        </li>
-                                    )
-                                ) : (
-                                    <li className={showSearchList ? "" : "hidden-search"}>No matches found</li>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="info-top">
-                        <div>
-                            <div className="stock-title">
-                                <h1>{info["Name"]} ({symbol})</h1>
-                                {/* <button onClick={() => addToWatchlist()}>
-                                    <i class="fa-regular fa-star"></i>
-                                    <span>Add to Watchlist</span>
-                                </button> */}
-                                <OpenModalButton modalComponent={<AddToWatchlist symbol={symbol} />}
-                                    buttonText={(
-                                        <>
-                                            <i class="fa-regular fa-star"></i>
-                                            <span>Add to Watchlist</span>
-                                        </>
-                                    )}
-                                />
-                                <button onClick={() => history.push(`/trading/${symbol}`)} id="trade-button">
-                                    <i class="fa-solid fa-chart-line"></i>
-                                    <span> Trade</span>
-                                </button>
-                            </div>
-                            <h5>{info["Exchange"]} • {info["Sector"]} • {info["Industry"]}</h5>
-                        </div>
-                        <div className="stock-return">
-                            <h4>${formatCurrentPrice(pricesArr[pricesArr.length - 1]['4. close'])}</h4>
-                            <div id={calculateStockReturn(pricesArr) > 0 ? "return-positive" : "return-negative"}>
-                                {calculateStockReturn(pricesArr)}%
+                            <div className="details-top-content">
+                                <div className="info-top">
+                                    <div>
+                                        <div className="stock-title">
+                                            <h1>{info["Name"]} ({symbol})</h1>
+                                            <OpenModalButton modalComponent={<AddToWatchlist symbol={symbol} />}
+                                                buttonText={(
+                                                    <>
+                                                        <i class="fa-regular fa-star"></i>
+                                                        <span>Add to Watchlist</span>
+                                                    </>
+                                                )}
+                                            />
+                                            <button onClick={() => history.push(`/trading/${symbol}`)} id="trade-button">
+                                                <i class="fa-solid fa-chart-line"></i>
+                                                <span> Trade</span>
+                                            </button>
+                                        </div>
+                                        <h5>{info["Exchange"]} • {info["Sector"]} • {info["Industry"]}</h5>
+                                    </div>
+                                    <div className="stock-return">
+                                        <h4>${formatCurrentPrice(pricesArr[pricesArr.length - 1]['4. close'])}</h4>
+                                        <div id={calculateStockReturn(pricesArr) > 0 ? "return-positive" : "return-negative"}>
+                                            {calculateStockReturn(pricesArr)}%
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="info-stats">
+                                    <div className="info-stats-col">
+                                        <div className="info-stat-item">
+                                            <h4>Market Cap</h4>
+                                            <h5>{formatBillions(info["MarketCapitalization"])}B</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>52 Week Low</h4>
+                                            <h5>{info["52WeekLow"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>52 Week High</h4>
+                                            <h5>{info["52WeekHigh"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Analyst Target Price</h4>
+                                            <h5>{info["AnalystTargetPrice"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Beta</h4>
+                                            <h5>{info["Beta"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>PE Ratio (TTM)</h4>
+                                            <h5>{info["PERatio"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Forward PE</h4>
+                                            <h5>{info["ForwardPE"]}</h5>
+                                        </div>
+                                    </div>
+                                    <div className="info-stats-col">
+                                        <div className="info-stat-item">
+                                            <h4>EV/EBITDA</h4>
+                                            <h5>{info["EVToEBITDA"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Price/Book Ratio</h4>
+                                            <h5>{info["PriceToBookRatio"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Return on Assets</h4>
+                                            <h5>{info["ReturnOnAssetsTTM"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Return on Equity</h4>
+                                            <h5>{info["ReturnOnEquityTTM"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Dividend Per Share</h4>
+                                            <h5>{info["DividendPerShare"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Dividend Yield</h4>
+                                            <h5>{info["DividendYield"]}</h5>
+                                        </div>
+                                        <div className="info-stat-item">
+                                            <h4>Ex-Dividend Date</h4>
+                                            <h5>{info["ExDividendDate"]}</h5>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="info-graph-container">
-                        <div className="info-stats">
-                            <div className="info-stats-col">
-                                <div className="info-stat-item">
-                                    <h4>Market Cap</h4>
-                                    <h5>{formatBillions(info["MarketCapitalization"])}B</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>52 Week Low</h4>
-                                    <h5>{info["52WeekLow"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>52 Week High</h4>
-                                    <h5>{info["52WeekHigh"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Analyst Target Price</h4>
-                                    <h5>{info["AnalystTargetPrice"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Beta</h4>
-                                    <h5>{info["Beta"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>PE Ratio (TTM)</h4>
-                                    <h5>{info["PERatio"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Forward PE</h4>
-                                    <h5>{info["ForwardPE"]}</h5>
-                                </div>
-                            </div>
-                            <div className="info-stats-col">
-                                <div className="info-stat-item">
-                                    <h4>EV/EBITDA</h4>
-                                    <h5>{info["EVToEBITDA"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Price/Book Ratio</h4>
-                                    <h5>{info["PriceToBookRatio"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Return on Assets</h4>
-                                    <h5>{info["ReturnOnAssetsTTM"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Return on Equity</h4>
-                                    <h5>{info["ReturnOnEquityTTM"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Dividend Per Share</h4>
-                                    <h5>{info["DividendPerShare"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Dividend Yield</h4>
-                                    <h5>{info["DividendYield"]}</h5>
-                                </div>
-                                <div className="info-stat-item">
-                                    <h4>Ex-Dividend Date</h4>
-                                    <h5>{info["ExDividendDate"]}</h5>
-                                </div>
-                            </div>
-                        </div>
                         <div className="graph-container">
                             <IndexPriceChart dataObj={pricesObj[`${symbol}`]} title="" lineColor="#008A05" />
                         </div>
